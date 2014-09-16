de32c3d10bed87d2ab355f7a7da893f33dce9538
Pau Fracés <pfraces@gmail.com>
2014-09-16 21:40:34 +0200

    added string, number and arrayLike checks

--- is.js
+++ is.js
@@ -5,7 +5,22 @@ define('mu.is', function () {
     return typeof arg !== 'undefined';
   };
   
+  var isString = function (arg) {
+    return typeof arg === 'string';
+  };
+  
+  var isNumber = function (arg) {
+    return typeof arg === 'number';
+  };
+  
+  var isArrayLike = function (arg) {
+    return arg && isNumber(arg.length);
+  };
+  
   return {
-    defined: isDefined
-  }
+    defined: isDefined,
+    string: isString,
+    number: isNumber,
+    array: isArrayLike
+  };
 });
