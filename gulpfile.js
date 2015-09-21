f2263c5ca8c779c4dfd8e587733d78f18ee56794
Pau Fracés <pfraces@gmail.com>
2015-09-21 04:19:26 +0200

    added test runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,54 @@
 var gulp   = require('gulp'),
     concat = require('gulp-concat-sourcemap'),
-    eslint = require('gulp-eslint')
+    eslint = require('gulp-eslint'),
+    mocha  = require('gulp-mocha'),
     del    = require('del');
 
 gulp.task('clean', function () {
-  return del('output');
+  return del('dist');
 });
 
 gulp.task('lint', ['clean'], function () {
-  return gulp.src('./src/**/*.js')
+  var src = [
+    './src/**/*.js',
+    './test/**.spec.js'
+  ];
+
+  return gulp.src(src)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
 });
 
-gulp.task('dependencies', ['lint'], function () {
-  var dependencies = [];
-
-  return gulp.src(dependencies)
-    .pipe(concat('dependencies.js', { sourcesContent: true }))
-    .pipe(gulp.dest('./output/'));
-});
-
 gulp.task('scripts', ['lint'], function () {
   return gulp.src('./src/**/*.js')
     .pipe(concat('scripts.js', { sourcesContent: true }))
-    .pipe(gulp.dest('./output/'));
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('test', ['dependencies', 'scripts']);
+gulp.task('test-dependencies', ['lint'], function () {
+  var src = [
+    'define/src/define.js'
+  ].map(function (path) { return 'dependencies/' + path; });
+
+  return gulp.src(src)
+    .pipe(concat('test-dependencies.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('test-suite', ['scripts', 'test-dependencies'], function () {
+  var src = [
+    'dist/test-dependencies.js',
+    'dist/scripts.js',
+    'test/**.spec.js'
+  ];
+
+  return gulp.src(src)
+    .pipe(concat('test-suite.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('test', ['test-suite'], function () {
+  return gulp.src('dist/test-suite.js')
+    .pipe(mocha());
+});
---
+++ test/is.spec.js
@@ -0,0 +1,14 @@
+define.test(function (use) {
+  'use strict';
+
+  var expect = require('expect.js'),
+      isDefined = use('is.defined');
+
+  describe('is', function () {
+    describe('defined', function () {
+      it('should be a function', function () {
+        expect(isDefined).to.be.a('function');
+      });
+    });
+  });
+});
